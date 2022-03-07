9ed53aa4275c3496583e350223c3a54719fd5624
hahafei <>
2022-03-07 17:17:51 +0800

    质数

---
+++ day01/24.py
@@ -0,0 +1,10 @@
+emp = [["zhangsan", 18000], ["lisi", 16000], ["wangwu", 20000], ["maliu", 15000]]
+# for i in range(len(emp)):
+#     print("{}的月收入为{}元".format(emp[i][0], emp[i][1]))
+
+# for index, i in enumerate(emp):
+#     print("%s的月收入为%s" % (i[0], i[1]))
+
+# 找出工资最高的人叫啥
+
+
--- day01/26.py
+++ day01/26.py
@@ -1,5 +1,24 @@
-emp2 = (["zhangsan", 18000], ["lisi", 16000], ["wangwu", 2000], ["maliu", 15000])
-print(id(emp2))
-emp2[0][1] = 19000
-print(id(emp2))
+# emp2 = (["zhangsan", 18000], ["lisi", 16000], ["wangwu", 2000], ["maliu", 15000])
+# print(id(emp2))
+# emp2[0][1] = 19000
+# print(id(emp2))
 
+# for num in range(2, 1001):
+#     for i in range(2, num):  # num=2直接跳过这个循环
+#         if num % i == 0:
+#             break
+#     else:
+#         print(num, end=" ")
+
+
+list1 = []
+for i in range(2, 11):
+    list1.append(i)
+
+# for num in list1:
+for num in range(2, 11):
+    for j in range(2, num):
+        if num % j == 0:
+            list1.remove(num)
+            break
+print(list1)
